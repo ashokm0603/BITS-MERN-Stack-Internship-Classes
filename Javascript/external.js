bf8194148ee25eeddc48851972dbbf876ab84cf2
Ashoka M <ashokashokm2001@gmail.com>
2026-04-30 17:59:06 +0530

    day 8 and 9 class code

---
+++ Javascript/external.js
@@ -0,0 +1,71 @@
+num="java :${10+20}"
+console.log(num);
+num='javascript  ${10+20}'
+console.log(num);
+a=50;
+b=50
+num=`addition 
+of a  and b  : ${a+b}`
+console.log(num); 
+
+
+num=50;
+console.log(num);
+
+num=true
+console.log(num);
+
+class SampleExample{
+    display(){
+        console.log("This is non-static display method in sampleExample Class");
+    }
+    test(){
+        console.log("test method");
+    }
+}
+
+s1=new SampleExample()
+s1.display()
+s1.test()
+
+function additionExample(){
+    console.log(10+20);
+}
+additionExample()
+
+
+
+
+num=330;
+
+result=num%2==0?"Even Number ":"Odd Number"
+console.log(result);
+
+
+a=10;
+b='10' ; 
+c=null
+d=Symbol(10);
+e=15425484986763n
+let f;
+console.log("Type of variable a is:",typeof a);
+console.log("Type of variable b is:",typeof b);
+console.log("Type of variable c is:",typeof c);
+console.log("Type of variable d is:",typeof d);
+console.log("Type of variable e is:",typeof e);
+console.log("Type of variable f is:",typeof f);
+
+
+
+let a=10;
+a=20;
+let a=20;
+
+
+
+
+
+
+
+
+
