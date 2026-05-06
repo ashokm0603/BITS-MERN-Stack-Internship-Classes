8518c33a2dac38a5b8569ccd2f305b6aed8db276
Ashoka M <ashokashokm2001@gmail.com>
2026-05-06 14:15:43 +0530

    e-commerce frontend application intial push

---
+++ e-commerce-frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Dashboard from "./pages/Dashboard";
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/style.css"
+import LandingPage from "./pages/LandingPage";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
---
+++ e-commerce-frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import CarouselContainer from '../components/CarouselContainer'
+import DumiProducts from '../components/DumiProducts'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <CarouselContainer/>
+      <DumiProducts/>
+    </div>
+  )
+}
+
+export default LandingPage
